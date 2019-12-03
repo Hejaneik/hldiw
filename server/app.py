ae5eeef80a5790c2ede1d6d9683ef9d67d6be818
Hendrik Höfert <hendrik.hoefert@gmail.com>
2019-12-03 18:10:05 +0100

    started working on database and database outline

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
-import datetime
+from datetime import datetime
 
 # configuration
 DEBUG = True
@@ -29,6 +30,39 @@ RECORDS = [
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+db = SQLAlchemy(app)
+
+# model database tables
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=True)
+    password = db.Column(db.String(120), nullable=False)
+    user_since = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    email = db.Column(db.String(120), nullable=False)
+    posts = db.relationship('Delays', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'User({username}, {first_name}, {last_name})'
+
+admin = User(username='admin', first_name='Hendrik', last_name='Hoefert', password='admin', email='admin@example.com')
+lucas = User(username='cpastoncp', first_name='Lucas', last_name='Jansen', paswwort='password', email='lucasjansen@example.com')
+
+class Delays(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    late_person_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    #to resolve dependency issues
+    owner = db.relationship('User', foreign_keys=[owner_id])
+    late_person = db.relationship('User', foreign_keys=[late_person_id])
+    delay = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    excuse = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return f'Delay({owner}, {late_person}, {delay}, {date})'
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
