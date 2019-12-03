cec0f26499241fe164661a13851ddb585c0c1b34
Hendrik Höfert <hendrik.hoefert@gmail.com>
2019-12-03 11:54:44 +0100

    added new route with non-db mock data

--- server/app.py
+++ server/app.py
@@ -1,19 +1,51 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-#configuration
+import datetime
+
+# configuration
 DEBUG = True
+
+RECORDS = [
+    {
+        'name': 'Max Mustermann',
+        'time': '2 hours',
+        'date': datetime.datetime(2019, 12, 2),
+        'excuse': 'none'
+    },
+    {
+        'name': 'Max Mustermann',
+        'time': '3 hours',
+        'date': datetime.datetime(2019, 12, 3),
+        'excuse': 'laziness'
+    },
+    {
+        'name': 'Mux Mastermann',
+        'time': '2 hours',
+        'date': datetime.datetime(2019, 12, 1),
+        'excuse': 'Shopping in airsoft shop'
+    }
+]
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-#enable CORS
-CORS(app, resources={r'/*' : {'origins': '*'}})
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong')
 
+
+@app.route('/records', methods=['GET'])
+def all_records():
+    return jsonify({
+        'status': 'success',
+        'records': RECORDS
+    })
+
+
 if __name__ == '__main__':
     app.run()
-
