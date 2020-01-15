7c187a0943e60ca8c432e24dfb94b4d631a2a1a2
Hendrik Höfert <hendrik.hoefert@gmail.com>
2020-01-15 11:44:39 +0100

    added signup route

--- server/app/auth.py
+++ server/app/auth.py
@@ -1,8 +1,27 @@
 # auth blueprint
 
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from datetime import datetime
+
+import jwt
+
 from . import db, ma
+from .models import User, UserSchema
 
 auth = Blueprint('auth', __name__)
 
+user_schema = UserSchema()
+
 # routes will go here
+@auth.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    user = User.query.filter((User.email == data.email) | (User.username == data.username)).first()
+    if user:
+        return None, 406 # TODO flash correct error message
+
+    user = User(**data)
+    db.session.add(user)
+    db.session.commit()
+    return user_schema.jsonify(user), 201
