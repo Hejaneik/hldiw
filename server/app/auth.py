845f1c0372d06bab910c07725e5909ef11168389
Hendrik Höfert <hendrik.hoefert@gmail.com>
2020-03-22 13:16:11 +0100

    added verification wrapper

--- server/app/auth.py
+++ server/app/auth.py
@@ -38,8 +38,40 @@ def login():
     # create JSON Web Token
     token = jwt.encode({
         'sub': user.username,
-        'iat': datetime.utcnow(), # time the token was creaeted
+        'iat': datetime.utcnow(), # time the token was created
         'exp': datetime.utcnow() + timedelta(minutes=30)},
         current_app.config['SECRET_KEY'])
         
     return jsonify({'token': token.decode('UTF-8')})
+
+def token_required(f):
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registration and/or reauthetication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Token expried, reauthetication required',
+            'authenticated': False
+        }
+
+        if len(auth_headers) != 2:
+            return jsonify(invalid_msg), 401
+        
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, current_app.config['SECRET_KEY'])
+            user = User.query.filter_by(username=data['sub']).first()
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs) # TODO check this
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
