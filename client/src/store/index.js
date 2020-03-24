3fde50db748870b9ab81db72b8b9aa969546df3e
Hendrik Höfert <hendrik.hoefert@gmail.com>
2020-03-24 13:24:05 +0100

    added vuex actions for signing in and registering

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,10 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 // AJAX methods from api here
-import { fetchDelays, addDelay } from '@/api';
+import {
+  fetchDelays, addDelay, signin, register,
+} from '@/api';
+import { isValidJwt, EventBus } from '@/util';
 
 Vue.use(Vuex);
 
@@ -10,6 +13,7 @@ export default new Vuex.Store({
   state: {
     delays: [],
     jwt: '',
+    user: {},
   },
   mutations: {
     setDelays(state, payload) {
@@ -23,6 +27,26 @@ export default new Vuex.Store({
     },
     submitDelay(context, delay) {
       return addDelay(delay);
+    },
+    login(context, userData) {
+      context.commit('setUserData', { userData });
+      return signin(userData)
+        .then(res => context.commit('setJwt', { jwt: res.data }))
+        .catch((err) => {
+          // eslint-disable-next-line
+          console.log('Error Authenticating: ', err);
+          EventBus.$emit('failedAuthentication', err);
+        });
+    },
+    register(context, userData) {
+      context.commit('setUserData', { userData });
+      return register(userData)
+        .then(context.dispatch('login', { userData }))
+        .catch((err) => {
+          // eslint-disable-next-line
+          console.log('Error Registering: ', err);
+          EventBus.$emit('failedRegsitering', err);
+        });
     },
   },
   modules: {
