34b6fe154a1838244a83ac786a724c0b992aa279
Hendrik <hendrik.hoefert@gmail.com>
2020-03-28 14:02:44 +0100

    fixed init script

--- server/init_db_mock.py
+++ server/init_db_mock.py
@@ -1,16 +1,20 @@
-from app import db
+from app import db, create_app
 from app.models import Delay
 from datetime import datetime
 
-db.create_all()
+app = create_app()
 
-delay1 = Delay(1, 2, 12, datetime.utcnow(), 'no excuse')
-delay2 = Delay(1, 2, 13, datetime.utcnow(), 'excuse one')
-delay3 = Delay(1, 2, 14, datetime.utcnow(), 'excusez moi')
+with app.app_context():
+    # NOTE if the database URI points to a directory, this diretory has to exist. SQLAlchemy only creates the db file, not paths
+    db.create_all()
 
-db.session.add(delay1)
-db.session.add(delay2)
-db.session.add(delay3)
+    delay1 = Delay(1, 2, 12, datetime.utcnow(), 'no excuse')
+    delay2 = Delay(1, 2, 13, datetime.utcnow(), 'excuse one')
+    delay3 = Delay(1, 2, 14, datetime.utcnow(), 'excusez moi')
 
-db.session.commit()
+    db.session.add(delay1)
+    db.session.add(delay2)
+    db.session.add(delay3)
+
+    db.session.commit()
 
