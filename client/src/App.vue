824dc3d12d067c5cded1c63ccb98ebc7e2954d56
Hendrik <hendrik.hoefert@gmail.com>
2020-03-29 14:20:41 +0200

    restyled navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -82,11 +82,6 @@ $colors: (
   )
 );
 
-// Links
-$link: $primary;
-$link-invert: $primary-invert;
-$link-focus-border: $primary;
-
 // Import Bulma and Buefy styles
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
@@ -98,18 +93,5 @@ $link-focus-border: $primary;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,16 +1,36 @@
 <template>
-  <header>
-    <div id="nav">
-      <router-link id="logo" tag="p" to="/">HLDIW</router-link>
-      <span></span>
-      <router-link class="navitem" to="/">Dashboard</router-link>
-      <router-link class="navitem" to="/delays">Delays</router-link>
-      <router-link class="navitem" to="/friends">Friends</router-link>
-      <router-link class="navitem" to="/about">About</router-link>
-      <router-link class="navbutton" tag="button" to="/login">Login</router-link>
-      <router-link class="navbutton" tag="button" to="/register">Register</router-link>
-    </div>
-  </header>
+  <b-navbar type="is-secondary">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
+        />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">Dashboard</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/delays' }">Delays</b-navbar-item>
+      <b-navbar-dropdown label="Social" hoverable="true">
+        <b-navbar-item tag="router-link" :to="{ path: '/friends' }">Friends</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/addfriend' }">Add Friend</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/contact' }">Contact</b-navbar-item>
+      </b-navbar-dropdown>
+      <b-navbar-item tag="router-link" :to="{ path: '/about' }">About</b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button secondary-comp" tag="router-link"
+          :to="{ path: '/register' }">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light" tag="router-link" :to="{ path: '/login' }">Log in</a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
 
 <script>
@@ -19,85 +39,8 @@ export default {
 };
 </script>
 
-<style lang="scss">
-header {
-  width: 100%;
-  position: sticky;
-  top: 0;
-}
-
-#nav {
-  background-color: $secondary;
-  text-align: left;
-  /* min-height: 6vh; */
-  width: 100%;
-  display: flex;
-}
-
-#logo {
-  margin: auto 0;
-  color: #ffffff;
-  font-family: Arial;
-  font-size: 1.5em;
-  display: inline-block;
-  padding-left: 3%;
-  cursor: pointer;
-}
-
-a,
-button {
-  font-family: "Montserrat", sans-serif;
-  color: #dadada;
-  text-decoration: none;
-  font-size: 1.2em;
-  font-weight: 500;
-  padding: 10px 3% 10px 0;
-  height: 100%;
-  display: inline-block;
-  box-sizing: border-box;
-  transition: all 0.3s ease 0s;
-}
-
-span {
-  flex-grow: 2;
-}
-
-.navitem a:hover {
-  color: #00c8f5;
-}
-
-.nav-style {
-  list-style: none;
-}
-
-button {
-  background-color: rgba(0, 179, 219, 1);
-  /* background-color: #00b3db; */
-  color: #292929;
-  border: none;
-  border-radius: 50px;
-  cursor: pointer;
-  transition: all 0.3s ease 0s;
-  padding: 5px 25px;
-  font-size: 1em;
-  margin: auto 5% auto 0;
-}
-
-button:hover {
-  background-color: rgba(0, 179, 219, 0.8);
-  color: #292929;
-}
-
-.navbutton {
-  color: #292929 !important;
-}
-
-.router-link-exact-active {
-  /* background-color: #444444; */
-  color: #00a8ce;
-}
-
-#logo {
-    color: #ffffff !important;
-}
+<style lang="scss" scoped>
+  .secondary-comp {
+    background-color: $secondary-comp;
+  }
 </style>
